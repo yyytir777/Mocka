d81ca920a8765d5fdc335dbce4f2b1ddd88d2c46
Wonjae Lim <yyytir777@gmail.com>
2025-07-17 14:59:57 +0900

    test : create main GeneratorFactory test

--- jodag-core/src/test/java/jodag/JodagMainTest.java
+++ jodag-core/src/test/java/jodag/JodagMainTest.java
@@ -1,25 +1,32 @@
 package jodag;
 
 import jodag.generator.Generator;
+import jodag.generator.GeneratorFactory;
+import jodag.generator.common.EmailGenerator;
+import jodag.generator.common.LoremIpsumGenerator;
+import jodag.generator.common.NameGenerator;
+import jodag.generator.primitive.PrimitiveGenerator;
 import org.junit.jupiter.api.Test;
 
-public class JodagV2Test {
+public class JodagMainTest {
 
     @Test
     void test() {
         // string 반환
-        StringGenerator stringGenerator = GeneratorFactory.string();
-        String string = stringGenerator.get();
-        String string = stringGenerator.get(10);
+//        StringGenerator stringGenerator = GeneratorFactory.string();
+//        String string = stringGenerator.get();
+//        String string = stringGenerator.get(10);
 
 
         // email 반환
         EmailGenerator emailGenerator = GeneratorFactory.email();
         String email = emailGenerator.get();
+        System.out.println("email = " + email);
 
         // name 반환
         NameGenerator nameGenerator = GeneratorFactory.name();
         String name = nameGenerator.get();
+        System.out.println("name = " + name);
 
         // lorem ipsum 반환
         LoremIpsumGenerator loremIpsumGenerator = GeneratorFactory.loremIpsum();
@@ -30,10 +37,14 @@ public class JodagV2Test {
         Boolean bool = primitiveGenerator.getBoolean();
         Byte b = primitiveGenerator.getByte();
         Short s = primitiveGenerator.getShort();
-        Character c = primitiveGenerator.getCharcter();
+        Character c = primitiveGenerator.getCharacter();
         Integer i = primitiveGenerator.getInteger();
         Long l = primitiveGenerator.getLong();
         Float f = primitiveGenerator.getFloat();
         Double d = primitiveGenerator.getDouble();
+
+        Generator<String> generator = GeneratorFactory.getRegistableGenerator("asdf", "name.txt", String.class);
+        String s1 = generator.get();
+        System.out.println("s1 = " + s1);
     }
 }
