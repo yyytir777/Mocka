2eccfb22d4f61cfd0f4c591c6cfa67f4861eb322
Wonjae Lim <yyytir777@gmail.com>
2025-07-21 11:00:36 +0900

    test : jodag-spring main test

--- jodag-spring/src/test/java/jodag/JodagSpringMainTest.java
+++ jodag-spring/src/test/java/jodag/JodagSpringMainTest.java
@@ -2,23 +2,28 @@ package jodag;
 
 import jodag.entity.Member;
 import jodag.generator.Generator;
-import jodag.registry.SpringDataRegistry;
+import jodag.generator.SpringGeneratorFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
 
 @SpringBootTest(classes = TestConfig.class)
 @DisplayName("Jodag-Spring 메인 기능 테스트 코드")
 public class JodagSpringMainTest {
 
-    @Autowired
-    SpringDataRegistry dataRegistry;
-
     @Test
     void test() {
-        Generator<Member> generator = dataRegistry.getGenerator(Member.class);
-        Member member = generator.get();
-        System.out.println("member = " + member.toString());
+        Generator<Member> generator = SpringGeneratorFactory.getGenerator(Member.class);
+
+        for(int i = 0; i < 10; i++) {
+            Member member = generator.get();
+            System.out.println("member = " + member.toString());
+        }
+
+        List<String> generatorNames = SpringGeneratorFactory.getGeneratorNames();
+        System.out.println("generatorNames = " + generatorNames);
     }
 }
