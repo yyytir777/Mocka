a6a89dacee312fa5cf2d0a72db8407624d36929d
Wonjae Lim <yyytir777@gmail.com>
2025-07-29 17:07:46 +0900

    test : test code

--- jodag-spring/src/test/java/jodag/JodagSpringMainTest.java
+++ jodag-spring/src/test/java/jodag/JodagSpringMainTest.java
@@ -1,10 +1,13 @@
 package jodag;
 
+import jodag.entity.Experience;
 import jodag.entity.Member;
+import jodag.entity.TestEntity;
 import jodag.generator.Generator;
 import jodag.generator.SpringGeneratorFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
@@ -24,30 +27,38 @@ public class JodagSpringMainTest {
     }
 
     @Test
-    @DisplayName("Member.class를 자동으로 생성합니다.")
+    @DisplayName("Member 엔티티를 @Generate 등록 후, 생성합니다.")
     void test() {
         Generator<Member> generator = SpringGeneratorFactory.getGenerator(Member.class);
 
         Member member = generator.get();
-        System.out.println("member : " + member.toString());
-        assertThat(member.getId()).isNotNull();
+        System.out.println("member = " + member.toString());
+        assertThat(member.getId()).isNull(); // id는 JPA 생성전략에 따라 null로 설정 (@GeneratedValue(strategy = GenerationType.IDENTITY))
+        assertThat(member.getEmail()).isNotNull();
         assertThat(member.getName()).isNotNull();
 
         List<String> generatorNames = SpringGeneratorFactory.getGeneratorNames();
-        System.out.println("generatorNames : " + generatorNames);
+        System.out.println("generatorNames = " + generatorNames);
         assertThat("Member").isIn(generatorNames);
     }
 
     @Test
-    @DisplayName("Member.class를 10개 생성합니다.")
+    @DisplayName("Member 엔티티 인스턴스 10개 생성")
     void create_10_instances() {
         Generator<Member> generator = SpringGeneratorFactory.getGenerator(Member.class);
 
         for (int i = 0; i < 10; i++) {
             Member member = generator.get();
             System.out.println("member : " + member.toString());
-            assertThat(member.getId()).isNotNull();
             assertThat(member.getName()).isNotNull();
         }
+    }
+
+    @Test
+    @DisplayName("test 엔티티 @Generate 등록 및 인스턴스 생성")
+    void test_entity_class() {
+        Generator<TestEntity> generator = SpringGeneratorFactory.getGenerator(TestEntity.class);
+        TestEntity testEntity = generator.get();
+        System.out.println("testEntity = " + testEntity.toString());
     }
 }
