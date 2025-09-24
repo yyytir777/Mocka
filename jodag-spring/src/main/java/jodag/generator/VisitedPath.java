f18147ba8c3a8746c8525da75fa37379fca521f4
Wonjae Lim <yyytir777@gmail.com>
2025-09-24 10:17:48 +0900

    fix : 연관관계 매핑을 위한 유방향 클래스 wrapper 생성

--- jodag-spring/src/main/java/jodag/generator/VisitedPath.java
+++ jodag-spring/src/main/java/jodag/generator/VisitedPath.java
@@ -1,15 +1,10 @@
 package jodag.generator;
 
 
-import java.util.Objects;
-
-public class VisitedPath {
-    private final Class<?> classA;
-    private final Class<?> classB;
+public class VisitedPath extends Path {
 
     public VisitedPath(Class<?> classA, Class<?> classB) {
-        this.classA = classA;
-        this.classB = classB;
+        super(classA, classB);
     }
 
     public static VisitedPath of(Class<?> classA, Class<?> classB) {
@@ -17,30 +12,5 @@ public class VisitedPath {
                 classA.getName().compareTo(classB.getName()) <= 0 ? classA : classB,
                 classA.getName().compareTo(classB.getName()) <= 0 ? classB : classA
         );
-    }
-
-    public Class<?> getClassA() {
-        return classA;
-    }
-
-    public Class<?> getClassB() {
-        return classB;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof VisitedPath that)) return false;
-        return classA.equals(that.getClassA()) && classB.equals(that.getClassB());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(classA, classB);
-    }
-
-    @Override
-    public String toString() {
-        return "{" + "classA=" + classA.getSimpleName() + ", classB=" + classB.getSimpleName() + '}';
     }
 }
