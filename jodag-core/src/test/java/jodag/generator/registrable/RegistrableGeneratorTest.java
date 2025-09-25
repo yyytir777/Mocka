55ef858dfc9dcf4cf3521709ca045560b5ede827
Wonjae Lim <yyytir777@gmail.com>
2025-09-25 16:51:05 +0900

    test : fix passed test code

--- jodag-core/src/test/java/jodag/generator/registrable/RegistrableGeneratorTest.java
+++ jodag-core/src/test/java/jodag/generator/registrable/RegistrableGeneratorTest.java
@@ -1,8 +1,8 @@
-package jodag.generator.registable;
+package jodag.generator.registrable;
 
 import jodag.exception.GeneratorException;
 import jodag.generator.Generator;
-import jodag.generator.GeneratorFactory;
+import jodag.generator.factory.GeneratorFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
@@ -12,17 +12,18 @@ import static org.assertj.core.api.Assertions.*;
 
 
 @DisplayName("RegisterableGenerator 테스트")
-class RegisterableGeneratorTest {
+class RegistrableGeneratorTest {
 
+    private final GeneratorFactory generatorFactory = new GeneratorFactory();
     @BeforeEach
     void setUp() {
-        GeneratorFactory.clearRegistableGenerator();
+        generatorFactory.clearRegistrableGenerator();
     }
 
     @Test
     @DisplayName("getPath는 classpath를 지원합니다.")
     void get_path_with_classpath() {
-        Generator<String> generator = GeneratorFactory.getRegistableGenerator("test", "test.txt", String.class);
+        Generator<String> generator = generatorFactory.getRegistrableGenerator("test", "test.txt", String.class);
         String string = generator.get();
         assertThat(string).isNotNull();
     }
@@ -31,7 +32,7 @@ class RegisterableGeneratorTest {
     @Disabled
     @DisplayName("getPath는 절대경로를 지원합니다.")
     void get_path_with_absolute_path() {
-        Generator<String> generator = GeneratorFactory.getRegistableGenerator("test", "/Users/wonjae/Desktop/text.txt", String.class);
+        Generator<String> generator = generatorFactory.getRegistrableGenerator("test", "/Users/wonjae/Desktop/text.txt", String.class);
         String string = generator.get();
         assertThat(string).isNotNull();
     }
@@ -39,7 +40,7 @@ class RegisterableGeneratorTest {
     @Test
     @DisplayName("getPath는 classpath를 지원합니다. 만약 존재하지 않는 경로라면, GenerateException 예외가 발생합니다.")
     void get_path_with_classpath_exception() {
-        assertThatThrownBy(() -> GeneratorFactory.getRegistableGenerator("test", "asdf.txt", String.class))
+        assertThatThrownBy(() -> generatorFactory.getRegistrableGenerator("test", "asdf.txt", String.class))
                 .isInstanceOf(GeneratorException.class);
     }
 
@@ -47,7 +48,7 @@ class RegisterableGeneratorTest {
     @Disabled
     @DisplayName("getPath는 절대경로를 지원합니다. 만약 존재하지 않는 경로라면, GenerateException 예외가 발생합니다.")
     void get_path_with_absolute_path_throw_exception() {
-        assertThatThrownBy(() -> GeneratorFactory.getRegistableGenerator("test", "/Users/wonjae/Desktop/asdf.txt", String.class))
+        assertThatThrownBy(() -> generatorFactory.getRegistrableGenerator("test", "/Users/wonjae/Desktop/asdf.txt", String.class))
                 .isInstanceOf(GeneratorException.class);
     }
 }
