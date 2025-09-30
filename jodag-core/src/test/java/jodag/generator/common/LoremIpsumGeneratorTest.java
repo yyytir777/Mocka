eebad3e130474d91d81fa6a3e80ac293984a057f
Wonjae Lim <yyytir777@gmail.com>
2025-09-30 17:34:24 +0900

    test : add LoremIpsumGeneratorTest

---
+++ jodag-core/src/test/java/jodag/generator/common/LoremIpsumGeneratorTest.java
@@ -0,0 +1,62 @@
+package jodag.generator.common;
+
+import jodag.generator.factory.GeneratorFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("LoremIpsumGenerator Test")
+class LoremIpsumGeneratorTest {
+
+    private final GeneratorFactory generatorFactory = new GeneratorFactory();
+
+    @Test
+    @DisplayName("Verify the LoremIpsumGenerator retrieved through GeneratorFactory is the correct class")
+    void get_instance() {
+        // given & when
+        LoremIpsumGenerator loremIpsumGenerator = generatorFactory.asLoremIpsum();
+
+        // then
+        assertThat(loremIpsumGenerator).isInstanceOf(LoremIpsumGenerator.class);
+    }
+
+    @Test
+    @DisplayName("LoremIpsumGenerator is managed as a singleton")
+    void loremIpsumGenerator_is_singleton() {
+        // given & when
+        LoremIpsumGenerator loremIpsumGenerator1 = generatorFactory.asLoremIpsum();
+        LoremIpsumGenerator loremIpsumGenerator2 = generatorFactory.asLoremIpsum();
+
+        // then
+        assertThat(loremIpsumGenerator1).isNotNull();
+        assertThat(loremIpsumGenerator2).isNotNull();
+        assertThat(loremIpsumGenerator1).isSameAs(loremIpsumGenerator2);
+    }
+
+
+    @Test
+    @DisplayName("LoremIpsumGenerator returns random country")
+    void get_value_from_loremIpsumGenerator() {
+        // given & when
+        LoremIpsumGenerator loremIpsumGenerator = generatorFactory.asLoremIpsum();
+
+        // then
+        String loremIpsum = loremIpsumGenerator.get();
+        System.out.println("loremIpsum = " + loremIpsum);
+        assertThat(loremIpsum).isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("LoremIpsumGenerator returns random countries")
+    void get_values_from_loremIpsumGenerator() {
+        // given & when
+        LoremIpsumGenerator loremIpsumGenerator = generatorFactory.asLoremIpsum();
+
+        // then
+        for(int i = 0; i < 1000; i++) {
+            String loremIpsum = loremIpsumGenerator.get();
+            System.out.println("loremIpsum = " + loremIpsum);
+            assertThat(loremIpsum).isNotEmpty().isInstanceOf(String.class);
+        }
+    }}
