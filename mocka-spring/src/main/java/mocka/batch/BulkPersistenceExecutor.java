1488eb938aa59ae5545e9f7bcf4c6dc0bf8191d9
Wonjae Lim <yyytir777@gmail.com>
2025-10-20 15:23:19 +0900

    chore: temporarily remove bulk insert logic

--- mocka-spring/src/main/java/mocka/batch/BulkPersistenceExecutor.java
+++ mocka-spring/src/main/java/mocka/batch/BulkPersistenceExecutor.java
@@ -1,18 +1,18 @@
-package mocka.batch;
-
-import mocka.generator.EntityGenerator;
-import org.springframework.stereotype.Component;
-
-@Component
-public class BulkPersistenceExecutor<T> {
-
-    private final BulkPersistenceManager<T> bulkPersistenceManager;
-
-    public BulkPersistenceExecutor(BulkPersistenceManager<T> bulkPersistenceManager) {
-        this.bulkPersistenceManager = bulkPersistenceManager;
-    }
-
-    public void execute(EntityGenerator<T> generator, Long count, Class<T> entityType) throws InterruptedException {
-        bulkPersistenceManager.start(generator, count, entityType);
-    }
-}
+//package mocka.batch;
+//
+//import mocka.generator.EntityGenerator;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class BulkPersistenceExecutor<T> {
+//
+//    private final BulkPersistenceManager<T> bulkPersistenceManager;
+//
+//    public BulkPersistenceExecutor(BulkPersistenceManager<T> bulkPersistenceManager) {
+//        this.bulkPersistenceManager = bulkPersistenceManager;
+//    }
+//
+//    public void execute(EntityGenerator<T> generator, Long count, Class<T> entityType) throws InterruptedException {
+//        bulkPersistenceManager.start(generator, count, entityType);
+//    }
+//}
--- mocka-spring/src/main/java/mocka/batch/config/BulkExecutorDefaultConfiguration.java
+++ mocka-spring/src/main/java/mocka/batch/config/BulkExecutorDefaultConfiguration.java
@@ -1,33 +1,33 @@
-package mocka.batch.config;
-
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.task.ThreadPoolTaskExecutorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-
-@Configuration
-public class BulkExecutorDefaultConfiguration {
-
-    @Bean
-    @ConditionalOnMissingBean(name = "bulkProducerExecutor")
-    public ThreadPoolTaskExecutor bulkProducerExecutor() {
-        return new ThreadPoolTaskExecutorBuilder()
-                .corePoolSize(16)
-                .maxPoolSize(16)
-                .queueCapacity(10000)
-                .threadNamePrefix("Bulk-Producer-").build();
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(name = "bulkConsumerExecutor")
-    public ThreadPoolTaskExecutor bulkConsumerExecutor() {
-        return new ThreadPoolTaskExecutorBuilder()
-                .corePoolSize(16)
-                .maxPoolSize(24)
-                .queueCapacity(0)
-                .threadNamePrefix("Bulk-Consumer")
-                .build();
-    }
-}
+//package mocka.batch.config;
+//
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+//import org.springframework.boot.task.ThreadPoolTaskExecutorBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+//
+//
+//@Configuration
+//public class BulkExecutorDefaultConfiguration {
+//
+//    @Bean
+//    @ConditionalOnMissingBean(name = "bulkProducerExecutor")
+//    public ThreadPoolTaskExecutor bulkProducerExecutor() {
+//        return new ThreadPoolTaskExecutorBuilder()
+//                .corePoolSize(16)
+//                .maxPoolSize(16)
+//                .queueCapacity(10000)
+//                .threadNamePrefix("Bulk-Producer-").build();
+//    }
+//
+//    @Bean
+//    @ConditionalOnMissingBean(name = "bulkConsumerExecutor")
+//    public ThreadPoolTaskExecutor bulkConsumerExecutor() {
+//        return new ThreadPoolTaskExecutorBuilder()
+//                .corePoolSize(16)
+//                .maxPoolSize(24)
+//                .queueCapacity(0)
+//                .threadNamePrefix("Bulk-Consumer")
+//                .build();
+//    }
+//}
