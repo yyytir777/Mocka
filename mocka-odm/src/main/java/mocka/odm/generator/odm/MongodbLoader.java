fcbc6c0963c8a877f39b0424b0bb80b571f26bed
Wonjae Lim <yyytir777@gmail.com>
2026-02-22 16:38:18 +0900

    feat : implement Document class Loader

--- mocka-odm/src/main/java/mocka/odm/generator/odm/MongodbLoader.java
+++ mocka-odm/src/main/java/mocka/odm/generator/odm/MongodbLoader.java
@@ -2,18 +2,45 @@ package mocka.odm.generator.odm;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
-public class MongodbLoader implements ODMLoader {
+public class MongodbLoader implements BeanFactoryAware, ODMLoader {
 
-    private static final Logger logger = LoggerFactory.getLogger(MongodbLoader.class);
+    private static final Logger log = LoggerFactory.getLogger(MongodbLoader.class);
+    private BeanFactory beanFactory;
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+    }
 
     @Override
     public Set<Class<?>> load() {
-        return Collections.emptySet();
+        Long startMs = System.currentTimeMillis();
+        ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
+        scanner.addIncludeFilter(new AnnotationTypeFilter(Document.class));
+        String basePackage = AutoConfigurationPackages.get(beanFactory).get(0);
+        Set<Class<?>> candidates = scanner.findCandidateComponents(basePackage)
+                .stream().map(def -> {
+                    try {
+                        return Class.forName(def.getBeanClassName());
+                    } catch (ClassNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).collect(Collectors.toSet());
+
+        Long endMs = System.currentTimeMillis();
+        log.info("Finished scanning document classes in {}ms. Add {} documents in DocumentGeneratorFactory", (endMs - startMs), candidates.size());
+        return candidates;
     }
 }
